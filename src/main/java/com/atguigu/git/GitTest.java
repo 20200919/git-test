5e1610c997e82767459e923cec5fcf72dcf7799d
sf <sf@atguigu.com>
2022-05-11 22:10:32 +0800

    [新增功能](third master):

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("hello Git1!");
+        System.out.println("hello Git2!");
+        System.out.println("hello Git3!");
     }
 }
