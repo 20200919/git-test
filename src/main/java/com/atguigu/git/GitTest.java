20b5f6568d7076f46906a72ddb420002550b4b47
sf <sf@atguigu.com>
2022-05-11 21:33:44 +0800

    git.gitignore

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author sf
+ * @date 2022/5/11 - 21:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
